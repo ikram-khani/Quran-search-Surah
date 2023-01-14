6abee3b025d95d5613b87a65ff08c11c305c8bd5
Ikram Khan <anjaanikr@gmail.com>
2023-01-14 21:35:06 +0500

    initial commit

---
+++ quran-search.js
@@ -0,0 +1,38 @@
+function searchQuran() {
+  var surahNumber = document.getElementById("surah-number").value;
+  var ayahNumber = document.getElementById("ayah-number").value;
+  fetch("https://api.alquran.cloud/v1/surah/" + surahNumber)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(surah) {
+      var surahData = surah.data;
+      var results = document.getElementById("results");
+      results.innerHTML = "<h2>Surah " + surahData.number + ": " + surahData.name + "</h2>";
+
+      results.innerHTML += "<h6> The Surah starts in JUZ: " + surahData.ayahs[0].juz + "</h6>";
+      results.innerHTML += "<h6> Number of Ayahs: " + surahData.numberOfAyahs + "</h6>";
+      var foundAyah = null;
+      surahData.ayahs.forEach(function(ayah) {
+        if (ayah.numberInSurah == ayahNumber) {
+          foundAyah = ayah;
+        }
+      });
+      if (foundAyah) {
+        results.innerHTML += "<h3>Ayah " + foundAyah.number + ":</h3>";
+        results.innerHTML += "<h5>" + foundAyah.text + "</h5>";
+      } else {
+        results.innerHTML += "<p>Ayah not found in this surah</p>";
+      }
+      var firstAyah =surahData.ayahs[0];
+      var lastAyah=surahData.ayahs[surahData.ayahs.length-1];
+      var rangeOfSurah=document.getElementById("range-of-surah");
+      rangeOfSurah.innerHTML="<h5>The Surah starts from Ayah "+firstAyah.number+" to Ayah "+lastAyah.number+"</h5>";
+      
+
+      
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
